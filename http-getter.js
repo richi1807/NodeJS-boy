1ec3e572b9fdb34f6ab788242dd3d2fe18bc53a1
Sankalp Srivastava <richi18007@gmail.com>
2014-02-17 19:57:33 +0530

    Advanced Http getter

--- http-getter.js
+++ http-getter.js
@@ -6,3 +6,12 @@ setInterval(function(){
 		console.log(res.headers) ;
 	});
 } , 2000);
+
+
+var server  = http.createServer(function(req , res){
+	res.write("hello");
+	setTimeout(function(){
+		res.end("end");
+	} , 2000);	
+});
+server.listen(8000);
